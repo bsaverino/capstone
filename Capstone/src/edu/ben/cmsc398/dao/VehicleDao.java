a85a9d18c536ba0b5c7c5fdf7412fa05b4db1ea2
Tom <tomkhou@gmail.com>
2013-03-20 23:10:53 -0500

    getAllVehicle has been created

--- Capstone/src/edu/ben/cmsc398/dao/VehicleDao.java
+++ Capstone/src/edu/ben/cmsc398/dao/VehicleDao.java
@@ -3,11 +3,13 @@ package edu.ben.cmsc398.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import edu.ben.cmsc398.dao.*;
 import edu.ben.cmsc398.model.*;
 
-public class VehicleDao extends DBConnector{
-	
+public class VehicleDao extends DBConnector {
+
 	public void addVehicle(Vehicle vehicle) throws SQLException {
 		String make = vehicle.getMake();
 		String model = vehicle.getModel();
@@ -17,19 +19,29 @@ public class VehicleDao extends DBConnector{
 		String color = vehicle.getColor();
 		int year = vehicle.getYear();
 		int userId = vehicle.getUserId();
-		
-		String sql = "insert into vehicle (user_id, year, make, model, trim, trans, engine_size, color) values ("+userId+","+year+",'"+make+"','"+model+"','"+trim+"','"+trans+"',"+engine+",'"+color+"');";		
+
+		String sql = "insert into vehicle (user_id, year, make, model, trim, trans, engine_size, color) values ("
+				+ userId
+				+ ","
+				+ year
+				+ ",'"
+				+ make
+				+ "','"
+				+ model
+				+ "','"
+				+ trim + "','" + trans + "'," + engine + ",'" + color + "');";
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ps.executeUpdate();
 	}
-	
-//	public ArrayList<Vehicle> getUsersVehicles(int userId){
-//		String sql = "select vehicle_id from vehicle where user_id =" + userId +";";
-//		
-//		
-//		
-//	}
-	
+
+	// public ArrayList<Vehicle> getUsersVehicles(int userId){
+	// String sql = "select vehicle_id from vehicle where user_id =" + userId
+	// +";";
+	//
+	//
+	//
+	// }
+
 	public void updateVehicle(Vehicle vehicle) throws SQLException {
 		int userId = vehicle.getUserId();
 		String make = vehicle.getMake();
@@ -40,28 +52,55 @@ public class VehicleDao extends DBConnector{
 		String color = vehicle.getColor();
 		int year = vehicle.getYear();
 		int vehicleId = vehicle.getVehicleId();
-		
-		String sql = "update vehicle set userId ="+userId+",make='"+make+"', model='"+model+"', trim='"+trim+"', trans="+trans+",engine="+engine+",color='"+color+"',year="+year+" where vehicleId='"+vehicleId+"';";		
+
+		String sql = "update vehicle set userId =" + userId + ",make='" + make
+				+ "', model='" + model + "', trim='" + trim + "', trans="
+				+ trans + ",engine=" + engine + ",color='" + color + "',year="
+				+ year + " where vehicleId='" + vehicleId + "';";
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ps.executeUpdate();
 	}
 
 	public void deleteVehicle(int vehicleID) throws SQLException {
-		String sql = "Delete from vehicle where vehicleID="+vehicleID+";";		
+		String sql = "Delete from vehicle where vehicleID=" + vehicleID + ";";
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ps.executeUpdate();
 	}
 
-	public int getNewVehicleId() throws SQLException{
-		String sql = "select Max(vehicle_id) as vehicle_id from vehicle;";	
+	public int getNewVehicleId() throws SQLException {
+		String sql = "select Max(vehicle_id) as vehicle_id from vehicle;";
 		int vehicleId = 0;
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ResultSet rs = ps.executeQuery();
 		if (rs != null) {
 			while (rs.next()) {
-				vehicleId  = rs.getInt("vehicle_id");
+				vehicleId = rs.getInt("vehicle_id");
 			}
 		}
-		return vehicleId+1;
+		return vehicleId + 1;
+	}
+
+	public ArrayList<Vehicle> getAllVehicle(int userId) throws SQLException {
+		String sql = "select * from vehicle where user_id='" + userId + "';";
+		Vehicle vehicle = null;
+		ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>();
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		if (rs != null) {
+			while (rs.next()) {
+				String make = rs.getString("make");
+				String model = rs.getString("model");
+				String trim = rs.getString("trim");
+				String trans = rs.getString("trans");
+				int engine = rs.getInt("engine_size");
+				String color = rs.getString("color");
+				int year = rs.getInt("year");
+				int vehicleId = rs.getInt("vehicle_id");
+				vehicle = new Vehicle(make, model, trim, trans, engine, color,
+						year, vehicleId, userId);
+				vehicleList.add(vehicle);
+			}
+		}
+		return vehicleList;
 	}
 }
